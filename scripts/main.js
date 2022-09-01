bc8f57d85600a4a8fc794f53dca84e7e1bd54c94
Aytrucks <105306690+Aytrucks@users.noreply.github.com>
2022-08-31 22:13:03 -0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+const myHeading = document.querySelector('h3');
+myHeading.textContent = 'Hello world!';
+
+function multiply(num1,num2) {
+    let result = num1 * num2;
+    return result;
+  }
+  multiply(4, 7);
+  multiply(20, 20);
+  multiply(0.5, 3);
+    
